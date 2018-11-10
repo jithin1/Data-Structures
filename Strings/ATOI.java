c5428106f0c7e250fecdb9518a97316224bd6ae3
Jithin Babu <jithinbabu707@gmail.com>
2018-11-11 00:01:36 +0530

    Update ATOI.java

--- Strings/ATOI.java
+++ Strings/ATOI.java
@@ -3,18 +3,23 @@ LeetCode 8 :String to Integer (atoi)
 
 Implement atoi which converts a string to an integer.
 
-The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting
+from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as
+a numerical value.
 
-The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior
+of this function.
 
-If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str 
+is empty or it contains only whitespace characters, no conversion is performed.
 
 If no valid conversion could be performed, a zero value is returned.
 
 Note:
 
     Only the space character ' ' is considered as whitespace character.
-    Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
+    Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. If 
+    the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
 
 Example 1:
 
@@ -35,6 +40,7 @@ class Solution {
         
          long a=0;
          boolean sign = false;
+        
            int i=0;
              int digits = 0;
             
@@ -69,11 +75,11 @@ class Solution {
               else{                  
                 a = a*10 +temp;  
                   
-                  if(a != 0)
+                  if(a != 0)  // if leading zeroes are present then number ofdigits is 0
                   digits++;
               }
             
-            if( digits > 10)
+            if( digits > 10)  // if digits > 10 we have exceeded integer boundary
                 break;
         }
          
